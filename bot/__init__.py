5cb0de16ea5b51d8e10f3c0929daa4a4c602f363
asder45r <thehealthurdu@gmail.com>
2021-02-05 13:26:24 +0500

    Removed Unawanted First

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,21 +3,11 @@ import os
 import threading
 import time
 
-import aria2p
 import telegram.ext as tg
 from dotenv import load_dotenv
 import socket
 
 socket.setdefaulttimeout(600)
-
-botStartTime = time.time()
-if os.path.exists('log.txt'):
-    with open('log.txt', 'r+') as f:
-        f.truncate(0)
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
-                    level=logging.INFO)
 
 load_dotenv('config.env')
 
@@ -37,25 +27,8 @@ try:
 except KeyError:
     pass
 
-aria2 = aria2p.API(
-    aria2p.Client(
-        host="http://localhost",
-        port=6800,
-        secret="",
-    )
-)
-
-DOWNLOAD_DIR = None
 BOT_TOKEN = None
 
-download_dict_lock = threading.Lock()
-status_reply_dict_lock = threading.Lock()
-# Key: update.effective_chat.id
-# Value: telegram.Message
-status_reply_dict = {}
-# Key: update.message.message_id
-# Value: An object of Status
-download_dict = {}
 # Stores list of users and chats the bot is authorized to use in
 AUTHORIZED_CHATS = set()
 if os.path.exists('authorized_chats.txt'):
@@ -67,15 +40,9 @@ if os.path.exists('authorized_chats.txt'):
 try:
     BOT_TOKEN = getConfig('BOT_TOKEN')
     parent_id = getConfig('GDRIVE_FOLDER_ID')
-    DOWNLOAD_DIR = getConfig('DOWNLOAD_DIR')
-    if DOWNLOAD_DIR[-1] != '/' or DOWNLOAD_DIR[-1] != '\\':
-        DOWNLOAD_DIR = DOWNLOAD_DIR + '/'
-    DOWNLOAD_STATUS_UPDATE_INTERVAL = int(getConfig('DOWNLOAD_STATUS_UPDATE_INTERVAL'))
     OWNER_ID = int(getConfig('OWNER_ID'))
     AUTO_DELETE_MESSAGE_DURATION = int(getConfig('AUTO_DELETE_MESSAGE_DURATION'))
     USER_SESSION_STRING = getConfig('USER_SESSION_STRING')
-    TELEGRAM_API = getConfig('TELEGRAM_API')
-    TELEGRAM_HASH = getConfig('TELEGRAM_HASH')
 except KeyError as e:
     LOGGER.error("One or more env variables missing! Exiting now")
     exit(1)
@@ -93,15 +60,6 @@ try:
         IS_TEAM_DRIVE = False
 except KeyError:
     IS_TEAM_DRIVE = False
-
-try:
-    USE_SERVICE_ACCOUNTS = getConfig('USE_SERVICE_ACCOUNTS')
-    if USE_SERVICE_ACCOUNTS.lower() == 'true':
-        USE_SERVICE_ACCOUNTS = True
-    else:
-        USE_SERVICE_ACCOUNTS = False
-except KeyError:
-    USE_SERVICE_ACCOUNTS = False
 
 updater = tg.Updater(token=BOT_TOKEN,use_context=True)
 bot = updater.bot
